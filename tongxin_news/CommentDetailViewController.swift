cc08ebf3f5e640b8078fed18446d7841c3ee98ae
郭轩 <guoxuan@kakuxuandeMac-mini.local>
2015-08-26 13:41:27 +0800

    ok

--- tongxin_news/CommentDetailViewController.swift
+++ tongxin_news/CommentDetailViewController.swift
@@ -17,12 +17,14 @@ class CommentDetailViewController: UIViewController, UITableViewDataSource, UITa
     var mobile = ""
     var marketId = ""
 
+    @IBOutlet weak var tvCommentDetail: UITableView!
     @IBOutlet weak var navBarCommentetail: UINavigationBar!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.navBarCommentetail.topItem?.title = group + " - " + market
+        getComments()
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,6 +43,7 @@ class CommentDetailViewController: UIViewController, UITableViewDataSource, UITa
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("CommentDetailCell", forIndexPath: indexPath) as! CommentDetailVCTableViewCell
         
+        cell.textLabel?.numberOfLines = 0
         cell.lblCommentDetailDate.text = products[indexPath.row].3
         cell.textLabel?.text = products[indexPath.row].2
         cell.imageView?.hnk_setImageFromURL(NSURL(string: products[indexPath.row].0))
@@ -57,5 +60,38 @@ class CommentDetailViewController: UIViewController, UITableViewDataSource, UITa
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func btnRefresh(sender: AnyObject) {
+        getComments()
+        tvCommentDetail.reloadData()
+    }
+    
+    func getComments()
+    {
+        let hud = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        request(.GET, EndPoints.GetCommentHierarchy.rawValue, parameters: ["marketId": marketId, "method": "getProducts"])
+            .responseJSON { (request, response, data, error) in
+                MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
+                if let anError = error
+                {
+                    let alert = SKTipAlertView()
+                    alert.showRedNotificationForString("加载失败，请点击右上角刷新按钮！", forDuration: 2.0, andPosition: SKTipAlertViewPositionTop, permanent: false)
+                }
+                else if let data: AnyObject = data
+                {
+                    if let res = JSON(data).array
+                    {
+                        self.products.removeAll(keepCapacity: true)
+                        for item in res
+                        {
+                            if let i = item.dictionary
+                            {self.products.append((i["avatar"]!.stringValue, i["url"]!.stringValue, i["title"]!.stringValue, i["date"]!.stringValue, i["id"]!.stringValue))
+                            }
+                        }
+                        self.tvCommentDetail.reloadData()
+                    }
+                }
+        }
+    }
 
 }
