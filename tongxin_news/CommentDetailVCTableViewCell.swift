1a98b9ff3fe29693af5539f5a99be9d36c0b3195
郭轩 <guoxuan@kakuxuandeMac-mini.local>
2015-08-26 10:07:26 +0800

    ok

---
+++ tongxin_news/CommentDetailVCTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentDetailVCTableViewCell.swift
+//  tongxin_news
+//
+//  Created by 郭轩 on 15/8/26.
+//  Copyright (c) 2015年 郭轩. All rights reserved.
+//
+
+import UIKit
+
+class CommentDetailVCTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
