6fded9fb68d39f0c57dbaebbd60c4d8464e64ad4
郭轩 <guoxuan@kakuxuandeMac-mini.local>
2015-08-27 11:25:04 +0800

    ok

--- tongxin_news/EndPoints.swift
+++ tongxin_news/EndPoints.swift
@@ -10,10 +10,10 @@ import Foundation
 
 enum EndPoints: String
 {
-    case SignIn = "http://172.20.68.245:8077/Handlers/LoginHandler.ashx"
-    case UserSet = "http://172.20.68.245:8077/Handlers/UserInfoHandler.ashx"
-    case InBoxMsg = "http://172.20.68.245:8077/Handlers/InboxMsgHandler.ashx"
-    case GetProductHierarchy = "http://172.20.68.245:8077/Handlers/XHMarketHandler.ashx"
-    case GetPrices = "http://172.20.68.245:8077/Handlers/PriceHandler.ashx"
-    case GetCommentHierarchy = "http://172.20.68.245:8077/Handlers/PLHandler.ashx"
+    case SignIn = "http://api.shtx.com.cn/Handlers/LoginHandler.ashx"
+    case UserSet = "http://api.shtx.com.cn/Handlers/UserInfoHandler.ashx"
+    case InBoxMsg = "http://api.shtx.com.cn/Handlers/InboxMsgHandler.ashx"
+    case GetProductHierarchy = "http://api.shtx.com.cn/Handlers/XHMarketHandler.ashx"
+    case GetPrices = "http://api.shtx.com.cn/Handlers/PriceHandler.ashx"
+    case GetCommentHierarchy = "http://api.shtx.com.cn/Handlers/PLHandler.ashx"
 }
